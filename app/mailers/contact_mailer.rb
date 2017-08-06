69bb435663d58f6ce7a5c3dc3cca23a7343f1f03
Aadi Malaviya <aadimalaviya@outlook.com>
2017-08-06 19:49:37 +0000

    Chabged deliver method

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,11 +1,11 @@
 class ContactMailer < ActionMailer::Base
-    default to: "aadimalaviya@outlook.com"
+    default to: "app73856317@heroku.com"
     
-    def contact_email(name,email,body)
+    def contact_email(name,email, body)
         @name = name
         @email = email
         @body = body
         
-        mail(from: email,subject: "Contact Form Message")
+        mail(from: 'app73856317@heroku.com',subject: "Contact Form Message")
     end
 end
