d4a448abda99fc8ee9824d73a5d7c63b93d3a2bc
Aadi Malaviya <aadimalaviya@outlook.com>
2017-08-07 01:04:07 +0000

    Aaded API key

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ActionMailer::Base
-    default to: "app73856317@heroku.com"
+    default to: "aadimalaviya@outlook.com"
     
     def contact_email(name,email, body)
         @name = name
