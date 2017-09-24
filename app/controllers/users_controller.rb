fe99b53103fd7154e84bc554201e7f552646dac1
Aadi Malaviya <aadimalaviya@outlook.com>
2017-09-24 23:28:13 +0000

    Improved user edit profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,17 @@
 class UsersController < ApplicationController
     before_action :authenticate_user!
     
+    def show
+        @user = User.find( params[:id] )
+    end
+    
+    def edit
+        
+    end
+    
     def index
         @users = User.includes(:profile)
     end
     
-    def show
-        @user = User.find( params[:id] )
-    end
+    
 end
