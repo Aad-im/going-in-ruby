98d43a07487384024874ab6aea68f6920b52660e
Aadi Malaviya <aadimalaviya@outlook.com>
2017-10-29 17:42:09 +0000

    Fixes

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,2 @@
-Rails.configuration.stripe = {
-  publishable_key: Rails.application.secrets.stripe_publishable_key,
-  secret_key:      Rails.application.secrets.stripe_secret_key
-}
-
-Stripe.api_key = Rails.configuration.stripe[:secret_key]
+Stripe.api_key = ENV["stripe_api_key"]
+STRIPE_PUBLIC_KEY = ENV["stripe_publishable_key"]
